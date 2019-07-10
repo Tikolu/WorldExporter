6620d3d94ca047467d0ecbd17c50964c0352a92c
Wiktor Owczarek <tikolu43@gmail.com>
2019-07-11 00:11:42 +0200

    Create installer.cmd

---
+++ files/driver/installer.cmd
@@ -0,0 +1,24 @@
+@ECHO OFF
+
+:: Minecraft PE Internal World Exporter (driver installer)
+::
+:: Created by Tikolu - http://tikolu.net16.net
+:: Report issues to tikolu43@gmail.com
+
+IF "%1"=="install" GOTO ARCH
+ECHO To use the World Exporter, run exporter.cmd from the root folder.
+ECHO Press enter to exit.
+PAUSE>NUL
+EXIT /B
+:ARCH
+IF DEFINED programfiles(x86) GOTO x64
+:x86
+ECHO(
+PING localhost -n 1 >NUL
+START /wait GoogleAndroidDriver86 /f 2>NUL
+EXIT /B
+:x64
+ECHO(
+PING localhost -n 1 >NUL
+START /wait GoogleAndroidDriver64 /f 2>NUL
+EXIT /B
