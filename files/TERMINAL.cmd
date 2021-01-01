3e885320623257fdc8fdf25c6c5df71c1769d66b
Wiktor Owczarek <tikolu43@gmail.com>
2021-01-01 23:26:21 +0000

    Updated to version 2.8

--- files/TERMINAL.cmd
+++ files/TERMINAL.cmd
@@ -9,49 +9,61 @@ TITLE Minecraft PE World Exporter - Terminal
 ECHO This Terminal is for diagnostic purposes only.
 ECHO To use the World Exporter, run exporter.cmd from the root folder.
 :0
+CD %~dp0
 ECHO.
 ECHO.
-ECHO 1 - Get connected device information
-ECHO 2 - Check if Minecraft is installed
-ECHO 3 - Send backup request to device
-ECHO 4 - Extract backup
-ECHO 5 - Extract archive
-ECHO 6 - Check size of backup and archive
-ECHO 7 - Display "minecraftWorlds" folder
-ECHO 8 - Manually enter commands
+ECHO.
+ECHO 1 - Restart ADB
+ECHO 2 - Get connected device information
+ECHO 3 - Check if Minecraft is installed
+ECHO 4 - Send backup request to device
+ECHO 5 - Extract backup
+ECHO 6 - Extract archive
+ECHO 7 - Check size of backup and archive
+ECHO 8 - Display "minecraftWorlds" folder
+ECHO 9 - Stop ADB
+ECHO.
+ECHO 10 - Manually enter commands
+ECHO.
 ECHO.
 SET input=8
 SET /p input="Enter your choice: "
-IF %input% GTR 8 GOTO 8
+IF %input% GTR 9 GOTO 10
 IF %input% LSS 1 GOTO 8
 GOTO %input%
 
 :1
-adb0 get-state
+adb0 kill-server
+adb0 start-server
 PAUSE
 GOTO 0
 
 :2
-adb0 shell pm path com.mojang.minecraftpe
+adb0 get-state
 PAUSE
 GOTO 0
 
 :3
-adb0 backup -noapk com.mojang.minecraftpe>NUL
+adb0 shell pm path com.mojang.minecraftpe
 PAUSE
 GOTO 0
 
 :4
-java -jar abe.jar unpack backup.ab backup.tar
+adb0 backup -noapk com.mojang.minecraftpe
 PAUSE
 GOTO 0
 
 :5
-7zip x -y -obackup backup.tar
+java -jar abe.jar unpack backup.ab backup.tar
 PAUSE
 GOTO 0
 
 :6
+7zip x -y -obackup backup.tar
+PAUSE
+GOTO 0
+
+:7
 ECHO Size of backup.ab:
 CALL getFileSize.cmd backup.ab
 ECHO Size of backup.tar:
@@ -59,13 +71,18 @@ CALL getFileSize.cmd backup.tar
 PAUSE
 GOTO 0
 
-:7
+:8
 ECHO Opening folder in Explorer
 explorer "backup\apps\com.mojang.minecraftpe\r\games\com.mojang\minecraftWorlds"
 GOTO 0
 
-:8
+:9
+adb0 kill-server
+PAUSE
+GOTO 0
+
+:10
 ECHO.
 SET /p input="%CD%>"
 %input%
-GOTO 8
+GOTO 10
